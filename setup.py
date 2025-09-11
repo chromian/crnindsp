a5959d52bd07b8827acafc3ba082439c56cec5c2
YongJin H. <38165066+chromian@users.noreply.github.com>
2025-09-11 16:12:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy as np
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
 extensions = [
-    Extension("crnindsp",
-              ["src/cycrnindsp.pyx"],
-              include_dirs=[np.get_include()],
-              extra_compile_args = ["-w"]
+    Extension(
+        "crnindsp.crnindsp",
+        ["src/crnindsp/cycrnindsp.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-w"]
     )
 ]
 
 setup(
-    name = 'crnindsp',
-    version = '0.0.3',
-    author = 'Yong-Jin Huang',
-    ext_modules = cythonize(extensions, annotate = True),
+    name='crnindsp',
+    version='0.0.3',
+    author='Yong-Jin Huang',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    ext_modules=cythonize(extensions, annotate=True, language_level=3),
     include_dirs=[np.get_include()],
+    zip_safe=False,
 )
