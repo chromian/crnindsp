e17879900ca2c460a9ff63902580a25bf6be25b0
YongJin H. <38165066+chromian@users.noreply.github.com>
2026-01-22 16:52:55 +0900

    Refactor setup.py to use distutils and add requirements
    Updated package version and added install_requires for numpy and scipy.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import find_packages
+from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 import Cython.Compiler.Options
@@ -7,20 +9,27 @@ Cython.Compiler.Options.annotate = True
 
 extensions = [
     Extension(
-        "crnindsp.crnindsp",
+        "crnindsp",
         ["src/crnindsp/cycrnindsp.pyx"],
         include_dirs=[np.get_include()],
         extra_compile_args=["-w"]
     )
 ]
 
+INSTALL_REQUIRES = [
+    'numpy>=1.24.4',
+    'scipy>=1.10.1',
+]
+
 setup(
     name='crnindsp',
-    version='0.0.4',
+    version='0.0.5',
     author='Yong-Jin Huang',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    ext_modules=cythonize(extensions, annotate=True, language_level=3),
+    ext_modules = cythonize(extensions, annotate = True),
     include_dirs=[np.get_include()],
     zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    python_requires='>=3',
 )
